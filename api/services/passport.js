b2e4612f65e89a25579aa83472929b61c1f31506
Tim Whitacre <tim@timw.co>
2016-02-15 14:08:09 -0500

    fixed stupid mistake

--- api/services/passport.js
+++ api/services/passport.js
@@ -15,7 +15,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_ID,
     clientSecret: process.env.GOOGLE_SECRET,
-    callbackURL: "http://xcourse.co//auth/google/callback"
+    callbackURL: "http://xcourse.co/auth/google/callback"
   },
   function(accessToken, refreshToken, profile, cb) {
 
